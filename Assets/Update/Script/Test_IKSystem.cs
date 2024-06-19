ddd436713173f047ea7e2eb810c57897edf3e2a8
allen2207153 <23ci0139@jec.ac.jp>
2024-06-19 19:04:46 +0900

    Update_スクリプト_手を繋ぐ(検討必要)
    手を繋ぐ機能がモデリングで追加しました。

---
+++ Assets/Update/Script/Test_IKSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_IKSystem : MonoBehaviour
+{
+    protected Animator animator;
+
+    public bool ikActive = true;
+    public Transform rightHandObj = null;
+    public Transform leftHandObj = null;
+    public Transform lookObj = null;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            // 如果 IK 被激活
+            if (ikActive)
+            {
+                // 設置目標位置和權重
+                if (lookObj != null)
+                {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }
+
+                // 設置右手目標位置和權重
+                if (rightHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                }
+
+                // 設置左手目標位置和權重
+                if (leftHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
+                }
+            }
+            // 如果 IK 被禁用，重置權重
+            else
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
