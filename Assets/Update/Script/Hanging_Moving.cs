64193f80fdbf52e0138c70c822bd6b8b222adb89
allen2207153 <23ci0139@jec.ac.jp>
2024-07-08 22:33:50 +0900

    Update_更新フォルダ追加
    アニメーション＿歩き
    お兄さん（仮）
    オブジェクト＿小物
    オブジェクト＿背景＿風車
    手を繋ぐ機能

---
+++ Assets/Update/Script/Hanging_Moving.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Hanging_Moving : MonoBehaviour
+{
+    public Transform man;
+    public Transform girl;
+    public float followSpeed = 2f;
+    private bool ikOn;
+    public Vector3 initialOffset;
+
+     void Awake()
+    {
+        ikOn = GetComponent<Test_IKSystem>().ikActive;
+    }
+
+     void Update()
+    {
+        if(ikOn)
+        {
+            girl.position = Vector3.Lerp(girl.position, man.position + initialOffset, followSpeed * Time.deltaTime);
+            Debug.Log("isHanging");
+        }
+    }
+}
