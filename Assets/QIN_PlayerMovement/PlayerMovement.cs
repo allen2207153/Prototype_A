09b9fd3c5a349e2d257d58469677cc673703d4b5
haku <51871245+Trouble-free@users.noreply.github.com>
2024-07-26 16:37:32 +0900

    押す機能を更新
    引っ張るは正常に見えますが、押すはちょっと変です。

--- Assets/QIN_PlayerMovement/PlayerMovement.cs
+++ Assets/QIN_PlayerMovement/PlayerMovement.cs
@@ -349,21 +349,26 @@ public class PlayerMovement : BChara
             }
 
             //20240723＿チョウハク
+            Vector3 playerDeltaMovement = _moveDirection *
+           ((_walkSpeedMin += _walkAddSpeed) < _walkSpeedMax ? _walkSpeedMin : _walkSpeedMax) *
+           _movementInput.magnitude *
+           Time.deltaTime;
+
+            Vector3 normalMovement = _moveDirection *
+           ((_walkSpeedMin += _walkAddSpeed) < _walkSpeedMax ? _walkSpeedMin : _walkSpeedMax) *
+           _movementInput.magnitude *
+           Time.deltaTime;
+            //移動入力の大きさを基に速度を調整し、プレイヤーを移動させます
+            _cCtrl.Move(normalMovement);
+
+            //20240723＿チョウハク
             if (_pushState)
             {
                 animator.ApplyBuiltinRootMotion();
                 animator.MatchTarget(_interactPoint.position, _interactPoint.rotation, AvatarTarget.Root,
                     new MatchTargetWeightMask(Vector3.one, 1f), 0.2f, 0.5f);
-            }
-            else
-            {
-                //20240723＿チョウハク
-                Vector3 playerDeltaMovement = _moveDirection *
-               ((_walkSpeedMin += _walkAddSpeed) < _walkSpeedMax ? _walkSpeedMin : _walkSpeedMax) *
-               _movementInput.magnitude *
-               Time.deltaTime;
-                //移動入力の大きさを基に速度を調整し、プレイヤーを移動させます
-                _cCtrl.Move(playerDeltaMovement);
+
+                
                 //20240723＿チョウハク
                 if (_movableObject)
                 {
@@ -373,6 +378,10 @@ public class PlayerMovement : BChara
                     playerDeltaMovement.y = 0f;
                     _movableObject.transform.Translate(playerDeltaMovement);
                 }
+            }
+            else
+            {
+               
             }
 
         }
@@ -479,7 +488,16 @@ public class PlayerMovement : BChara
     //追加時間：20240723＿チョウハク
     public void OnPush(InputAction.CallbackContext _ctx)
     {
-        _isPushPressed = _ctx.ReadValueAsButton();
+        //_isPushPressed = _ctx.ReadValueAsButton();
+
+        if (_ctx.phase == InputActionPhase.Started)
+        {
+            _isPushPressed = true;
+        }
+        else if (_ctx.phase == InputActionPhase.Canceled)
+        {
+            _isPushPressed = false;
+        }
     }
     /// <summary>
     /// プレイヤが押す機能の処理
@@ -495,11 +513,11 @@ public class PlayerMovement : BChara
                 _pushState = true;
             }
         }
-        else if (!_isPushPressed)
+        else
         {
             _pushState = false;
         }
-        else if (_pushState)
+        if (_pushState)
         {
             if (Vector3.Distance(_interactPoint.position, _cCtrl.transform.position) > 1f)
             {
