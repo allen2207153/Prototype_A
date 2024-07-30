f0c51ab11db8ee6f952c02ad68210956f0d44058
23CI0121 <gamecreatemulch@gmail.com>
2024-07-30 21:04:36 +0900

    扉に近づいたら開くようにした　
    とりあえずスクリプトは汚いけどギミックはできた

---
+++ Assets/Taniguchi_StateMachine/Scripts/ver1.0.1/DoorMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private Transform _target;
+    bool _isTrigger=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isTrigger == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("当たった");
+        if(other.gameObject.tag == "Player")
+        {
+            _isTrigger = true;
+        }
+        
+    }
+}
