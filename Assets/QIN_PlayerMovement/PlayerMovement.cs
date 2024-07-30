af49301eacea4a6357f72d3d64cf74abb11ef953
haku <51871245+Trouble-free@users.noreply.github.com>
2024-07-30 22:09:04 +0900

    押す機能を更新
    PlayerMovementに押す機能を更新しました。

--- Assets/QIN_PlayerMovement/PlayerMovement.cs
+++ Assets/QIN_PlayerMovement/PlayerMovement.cs
@@ -366,21 +366,20 @@ public class PlayerMovement : BChara
             }
 
             //20240723＿チョウハク
+            Vector3 playerDeltaMovement = _moveDirection *
+           ((_walkSpeedMin += _walkAddSpeed) < _walkSpeedMax ? _walkSpeedMin : _walkSpeedMax) *
+           _movementInput.magnitude *
+           Time.deltaTime;
+            //移動入力の大きさを基に速度を調整し、プレイヤーを移動させます
+            _cCtrl.Move(playerDeltaMovement);
+
+            //20240723＿チョウハク
             if (_pushState)
             {
-                animator.ApplyBuiltinRootMotion();
-                animator.MatchTarget(_interactPoint.position, _interactPoint.rotation, AvatarTarget.Root,
-                    new MatchTargetWeightMask(Vector3.one, 1f), 0.2f, 0.5f);
-            }
-            else
-            {
-                //20240723＿チョウハク
-                Vector3 playerDeltaMovement = _moveDirection *
-               ((_walkSpeedMin += _walkAddSpeed) < _walkSpeedMax ? _walkSpeedMin : _walkSpeedMax) *
-               _movementInput.magnitude *
-               Time.deltaTime;
-                //移動入力の大きさを基に速度を調整し、プレイヤーを移動させます
-                _cCtrl.Move(playerDeltaMovement);
+                //animator.ApplyBuiltinRootMotion();
+                //animator.MatchTarget(_interactPoint.position, _interactPoint.rotation, AvatarTarget.Root,
+                    //new MatchTargetWeightMask(Vector3.one, 1f), 0.2f, 0.5f);
+
                 //20240723＿チョウハク
                 if (_movableObject)
                 {
@@ -525,7 +524,17 @@ public class PlayerMovement : BChara
     //追加時間：20240723＿チョウハク
     public void OnPush(InputAction.CallbackContext _ctx)
     {
-        _isPushPressed = _ctx.ReadValueAsButton();
+        //_isPushPressed = _ctx.ReadValueAsButton();
+
+        if (_ctx.phase == InputActionPhase.Started)
+        {
+            _isPushPressed = true;
+        }
+        else if (_ctx.phase == InputActionPhase.Canceled)
+        {
+            _isPushPressed = false;
+        }
+
     }
     /// <summary>
     /// プレイヤが押す機能の処理
