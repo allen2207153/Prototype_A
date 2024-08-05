b92e639c0bf0b24422aec7892f0cc6bd29cb6c88
allen2207153 <23ci0139@jec.ac.jp>
2024-08-05 15:04:24 +0900

    Squashed commit of the following:
    commit f0c51ab11db8ee6f952c02ad68210956f0d44058
    Author: 23CI0121 <gamecreatemulch@gmail.com>
    Date:   Tue Jul 30 21:04:36 2024 +0900
    
        扉に近づいたら開くようにした　
    
        とりあえずスクリプトは汚いけどギミックはできた

---
+++ Assets/Taniguchi_StateMachine/Scripts/DoorTestPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTestPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        transform.Translate(x, 0, z);
+    }
+}
