8e63e21fde2aa1d23b53ce374873f5f758f4d546
haku <51871245+Trouble-free@users.noreply.github.com>
2024-08-06 20:09:19 +0900

    ノックバック追加
    追加スクリプト：KnockBack.cs
    追加オブジェクト：Enemy
    機能：プレイが敵に接触したら敵が弾き飛ばされる。

---
+++ Assets/Chou_PlayerInputSystem/Scripts/Misc/KnockBack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    [SerializeField] private float _knockBackForce = 50f;
+    [SerializeField] private GameObject _enemy;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _enemy = collision.gameObject;
+            EnemyKnockBack();
+        }
+    }
+
+    void EnemyKnockBack()
+    {
+        _enemy.transform.position += transform.forward * Time.deltaTime * _knockBackForce;
+    }
+}
