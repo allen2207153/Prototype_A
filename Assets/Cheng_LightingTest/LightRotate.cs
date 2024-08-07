7cf8110418c5f28829967450799e0a79b375e040
allen2207153 <23ci0139@jec.ac.jp>
2024-08-07 16:26:35 +0900

    Squashed commit of the following:
    commit 753d9d0cc31b3cfe2a388da66cb64f9ab0d3f868
    Author: Sorasora520 <lin.cheng1107@gmail.com>
    Date:   Mon Aug 5 17:04:11 2024 +0900
    
        時間をライティングによって表現する

---
+++ Assets/Cheng_LightingTest/LightRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotate : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0, 180)]public float lightAng;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.eulerAngles = new Vector3(lightAng, 0, 0);
+        
+    }
+}
---
+++ Assets/Cheng_LightingTest/SkyChange.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyChange : MonoBehaviour
+{
+    [SerializeField] LightRotate _directionLight;
+    float sunRot;
+    
+    [SerializeField] public Material day;
+    [SerializeField] public Material sunset;
+
+    [SerializeField] public float startSunset;
+    [SerializeField] public float endSunset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        sunRot = _directionLight.lightAng;
+
+        Material tempSkybox;
+
+        if(sunRot < startSunset)
+        {
+            //回転角度を指定範囲内で正規化
+            float t = Mathf.InverseLerp(startSunset, endSunset, sunRot);
+
+            //skyboxの補間
+            tempSkybox = LerpMaterials(day, sunset, t);
+        }
+        else
+        {
+            tempSkybox = day;
+        }
+
+        //skyboxを変更
+        RenderSettings.skybox = tempSkybox;
+    }
+
+    Material LerpMaterials(Material mat1, Material mat2, float t)
+    {
+        Material newMat = new Material(mat1);
+
+        //色を補間
+        Color mat1Color = mat1.GetColor("_Tint");
+        Color mat2Color = mat2.GetColor("_Tint");
+        Color tempColor = Color.Lerp(mat1Color, mat2Color, t);
+        newMat.SetColor("_Tint", tempColor);
+
+        return newMat;
+    }
+}
