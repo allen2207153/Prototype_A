63f01312b5927b1272f4891ec98d0061dc90b9dc
haku <51871245+Trouble-free@users.noreply.github.com>
2024-08-13 22:51:17 +0900

    攻撃アニメーションの仮実装
    仮でキーJを押すと攻撃アニメーションを出すようにしました。

--- Assets/Chou_PlayerInputSystem/Scripts/Player/MeleeAttack.cs
+++ Assets/Chou_PlayerInputSystem/Scripts/Player/MeleeAttack.cs
@@ -1,18 +1,40 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
+    Animator animator;
+
+    [SerializeField]
+    private float _coldTime = 2f;
+
+    [SerializeField]
+    private float _attackTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_attackTime <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                // attack
+                animator.SetTrigger("Attack");
+
+                _attackTime = _coldTime;
+            }
+        }
+        else
+        {
+            _attackTime -= Time.deltaTime;
+        }
     }
+
 }
