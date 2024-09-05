5541e3b06ea228d79065c56f33a2a3822b99dbbb
allen2207153 <23ci0139@jec.ac.jp>
2024-09-05 13:54:47 +0900

    Squashed commit of the following:
    commit fe5b3e7ef5cfef98a9d1052d62818b5afa23c186
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Thu Sep 5 13:51:55 2024 +0900
    
        Update_Stage
    
    commit 34f55a7949c3e3d7e45fb2101f8d960472eeb7c0
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Wed Sep 4 08:36:31 2024 +0900
    
        Update_Forest
    
    commit 9a42551b402bc37b5d005221723dca7c5ed2cd50
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Sun Sep 1 12:27:25 2024 +0900
    
        Update＿Building
    
    commit 4aa2938daa137cf5a9b32ff49381ad05048dd42d
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Fri Aug 30 20:19:58 2024 +0900
    
        Update_Modeling
    
    commit 8d138410d34ae43ca47b42f53f5b6d9f2f8e400c
    Author: Sorasora520 <lin.cheng1107@gmail.com>
    Date:   Fri Aug 30 01:11:46 2024 +0900
    
        撮影設置＋一部ステージ直す

---
+++ Assets/Wang/Script/WallTrigger.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour
+{
+    public GameObject[] leftWalls; // 左側の壁の配列
+    public Vector3 moveDistance; // 壁の移動距離
+    public float moveDuration = 1.0f; // 壁が移動する時間
+    public float delayBetweenMoves = 0.5f; // 壁が移動する間の時間差
+
+    private Vector3[] initialPositions; // 各壁の初期位置を記録する配列
+
+    void Start()
+    {
+        // 各壁の初期位置を記録
+        initialPositions = new Vector3[leftWalls.Length];
+        for (int i = 0; i < leftWalls.Length; i++)
+        {
+            initialPositions[i] = leftWalls[i].transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // プレイヤーがトリガーに触れた場合
+        if (other.GetComponent<CharacterController>() != null)
+        {
+            StartCoroutine(MoveWalls());
+        }
+    }
+
+    System.Collections.IEnumerator MoveWalls()
+    {
+        // 各壁を順番に移動させる
+        for (int i = 0; i < leftWalls.Length; i++)
+        {
+            yield return StartCoroutine(MoveWall(leftWalls[i], initialPositions[i] + moveDistance));
+
+            // 次の壁を移動させる前に時間差を待つ
+            yield return new WaitForSeconds(delayBetweenMoves);
+        }
+    }
+
+    System.Collections.IEnumerator MoveWall(GameObject wall, Vector3 targetPosition)
+    {
+        Vector3 startPosition = wall.transform.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < moveDuration)
+        {
+            wall.transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / moveDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        wall.transform.position = targetPosition;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // 壁の移動後の位置を表示
+        Gizmos.color = Color.green;
+        if (leftWalls != null)
+        {
+            for (int i = 0; i < leftWalls.Length; i++)
+            {
+                if (leftWalls[i] != null)
+                {
+                    Gizmos.DrawLine(leftWalls[i].transform.position, leftWalls[i].transform.position + moveDistance);
+                }
+            }
+        }
+    }
+}
