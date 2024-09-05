1bf088036bee609fdf503ae5c0080223e949f8a5
allen2207153 <23ci0139@jec.ac.jp>
2024-09-05 13:57:40 +0900

    Squashed commit of the following:
    commit fe5b3e7ef5cfef98a9d1052d62818b5afa23c186
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Thu Sep 5 13:51:55 2024 +0900
    
        Update_Stage
    
    commit 34f55a7949c3e3d7e45fb2101f8d960472eeb7c0
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Wed Sep 4 08:36:31 2024 +0900
    
        Update_Forest
    
    commit 9a42551b402bc37b5d005221723dca7c5ed2cd50
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Sun Sep 1 12:27:25 2024 +0900
    
        Update＿Building
    
    commit 4aa2938daa137cf5a9b32ff49381ad05048dd42d
    Author: allen2207153 <23ci0139@jec.ac.jp>
    Date:   Fri Aug 30 20:19:58 2024 +0900
    
        Update_Modeling
    
    commit 8d138410d34ae43ca47b42f53f5b6d9f2f8e400c
    Author: Sorasora520 <lin.cheng1107@gmail.com>
    Date:   Fri Aug 30 01:11:46 2024 +0900
    
        撮影設置＋一部ステージ直す

---
+++ Assets/Wang/Script/CharacterOnPlatform.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterOnPlatform : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Transform platformTransform;
+    private Vector3 previousPlatformPosition;
+    private bool isOnPlatform;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        // 检查角色是否站在带有“Platform”标签的物体上
+        if (hit.gameObject.CompareTag("Platform"))
+        {
+            // 记录平台Transform
+            if (!isOnPlatform)
+            {
+                platformTransform = hit.transform;
+                previousPlatformPosition = platformTransform.position;
+                isOnPlatform = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (isOnPlatform && platformTransform != null)
+        {
+            // 计算平台的移动量
+            Vector3 platformMovement = platformTransform.position - previousPlatformPosition;
+
+            // 将平台的移动量应用到角色上
+            characterController.Move(platformMovement);
+
+            // 更新平台的前一次位置
+            previousPlatformPosition = platformTransform.position;
+
+            // 检查角色是否仍在平台上
+            if (!characterController.isGrounded)
+            {
+                isOnPlatform = false;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // 当角色离开平台时，重置状态
+        if (other.CompareTag("Platform"))
+        {
+            isOnPlatform = false;
+            platformTransform = null;
+        }
+    }
+}
+
