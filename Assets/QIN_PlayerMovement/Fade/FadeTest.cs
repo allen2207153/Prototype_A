c651a3b1d7dce35d30d2032937cf525be59284ff
puyoco <93511276+puyoco@users.noreply.github.com>
2024-09-08 17:54:10 +0900

    シーンの切り替えとFadeIn,FadeOutを実装
    １，DOTweenを導入
    ２，FadeIn,FadeOutを実装
    ３、シーンの切り替えとFadeIn,FadeOutをカプセル化

---
+++ Assets/QIN_PlayerMovement/Fade/FadeTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTest : MonoBehaviour
+{
+    public static FadeTest Instance;
+    private void Awake()
+    {
+        //インスタンスの設定
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public FadeEvent _fadeEvent;
+#if UNITY_EDITOR
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            //_fadeEvent.FadeIn(1f);
+            SceneSwitcher.Instance.Loading(1f, "Stage_Demo");
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SceneSwitcher.Instance.Loading(1f, "Stage_Demo_FadeTest");
+        }
+    }
+#endif
+}
---
+++ Assets/QIN_PlayerMovement/ScenesSwitcher/SceneSwitcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public static SceneSwitcher Instance;
+    private void Awake()
+    {
+        //インスタンスの設定
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public FadeEvent _fadeEvent;
+
+    //[SerializeField] private float _fadeDuration;
+    IEnumerator LoadSceneCoroutine(float _fadeDuration, string sceneName)
+    {
+        while (true)
+        {
+            _fadeEvent.FadeIn(_fadeDuration);
+            yield return new WaitForSeconds(_fadeDuration + 0.4f);
+            Debug.Log("SceneLoadCompitteee!");//TODO: シーンを切り替え
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+            yield return new WaitUntil(() => operation.isDone);
+            //yield return new WaitForSeconds(_fadeDuration);
+            _fadeEvent.FadeOut(_fadeDuration + 0.2f);
+            yield break;
+        }
+    }
+    /// <summary>
+    /// シーンをFadeInと共に切り替え
+    /// </summary>
+    /// <param name="_fadeDuration">FadeIn変化スピード</param>
+    /// <param name="sceneName">シーンの名前</param>
+    public void Loading(float _fadeDuration,string sceneName)
+    {
+        StartCoroutine(LoadSceneCoroutine(_fadeDuration, sceneName));
+    }
+}
