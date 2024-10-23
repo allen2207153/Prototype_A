2d0ee1868a3d5b3888b02b387c5679c11afb7043
puyoco <93511276+puyoco@users.noreply.github.com>
2024-10-23 23:50:56 +0900

    Restart機能を実装
    Restart機能を実装、
    playerRespawn機能を追加。

--- Assets/QIN_PlayerMovement/PauseMenu/PauseMenuController.cs
+++ Assets/QIN_PlayerMovement/PauseMenu/PauseMenuController.cs
@@ -50,6 +50,11 @@ public class PauseMenuController : MonoBehaviour
             _oniisan.GetComponent<PlayerMovement>().IsPlayerPaused = false;
         }
     }
+    public void ReStart()
+    {
+        ResumeTheGame();
+        PlayerEvent.CallPlayerRespawn();
+    }
     public void BackToTitle()
     {
         GameIsPaused = false;
--- Assets/QIN_PlayerMovement/PlayerEvent.cs
+++ Assets/QIN_PlayerMovement/PlayerEvent.cs
@@ -9,9 +9,28 @@ public class PlayerEvent
         CheckHanging?.Invoke(checkHanging);
     }
 
-    public static event Action<Collider,bool> CheckCollider;
-    public static void CallCheckCollider(Collider collider,bool checkEnterOrExit)
+    public static event Action<Collider, bool> CheckCollider;
+    public static void CallCheckCollider(Collider collider, bool checkEnterOrExit)
     {
         CheckCollider?.Invoke(collider, checkEnterOrExit);
+    }
+
+    
+    public static event Action PlayerRespawn;
+    /// <summary>
+    /// プレイヤーと妹を前記録されたRespawnPointにRespawnする
+    /// </summary>
+    public static void CallPlayerRespawn()
+    {
+        PlayerRespawn?.Invoke();
+    }
+ 
+    public static event Action UpdateRespawnPoint;
+    /// <summary>
+    /// プレイヤーと妹の座標を記録し、RespawnPointに更新する
+    /// </summary>
+    public static void CallUpdateRespawnPoint()
+    {
+        UpdateRespawnPoint?.Invoke();
     }
 }
