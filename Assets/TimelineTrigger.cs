a5ddfb89d9aea4ba59e50d3b5a2dfeaf481ecf4d
allen2207153 <23ci0139@jec.ac.jp>
2024-12-09 06:00:27 +0900

    Update_machine

---
+++ Assets/TimelineTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector playableDirector;  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("imouto"))  
+        {
+            playableDirector.Play();  //Timelineをプレイする
+        }
+    }
+}
